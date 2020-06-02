67021b24d5236bc1c2305280ad8b0df87d5e1c97
JunquerGJ <38145180+JunquerGJ@users.noreply.github.com>
2020-06-02 16:51:30 +0200

    Users now can change their password

--- src/routes/users/user.controller.ts
+++ src/routes/users/user.controller.ts
@@ -1,9 +1,12 @@
 import UserService from './user.service';
 import { EntityController } from '../../shared/interfaces/interfaces';
 import HttpException from '../../routes/exceptions/HTTPException';
-import { PW_ALGORITHM,PW_KEYGENERATOR} from '../../utils/constants'
+import { PW_ALGORITHM, PW_KEYGENERATOR, JWT_SECRET } from '../../utils/constants'
 import * as express from 'express';
 import * as crypto from 'crypto'
+import * as jwt from 'jsonwebtoken';
+import DataStoredInToken from '../../routes/interfaces/dataStoredInToken.interface';
+
 import authMiddleware from '../../shared/middleware/auth.middleware'
 import permissionMiddleware from '../../shared/middleware/permission.middleware';
 
@@ -15,9 +18,38 @@ class UserController extends EntityController {
         this.router.post(this.path, authMiddleware, permissionMiddleware(["CREATE " + this.entityName]), this.addUser);
         this.router.all(this.path + "/*", authMiddleware);
         this.router.post(this.path + "/search", permissionMiddleware(["READ " + this.entityName]), this.getSome);
+        this.router.post(this.path + "/password", this.changePassword);
         this.router.get(this.path + "/:id", permissionMiddleware(["READ " + this.entityName]), this.get);
         this.router.patch(this.path + "/:id", permissionMiddleware(["UPDATE " + this.entityName]), this.modifyUser);
         this.router.delete(this.path + "/:id", permissionMiddleware(["DELETE " + this.entityName]), this.delete);
+    }
+
+    changePassword = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+        try {
+            if (!request.body.password) {
+                throw new HttpException(400, "Empty password field");
+            }
+            const keygen = PW_KEYGENERATOR
+            const key = crypto.scryptSync(keygen, 'salt', 24);
+            const iv = Buffer.alloc(16, 0);
+            const cipher = crypto.createCipheriv('aes-192-cbc', key, iv);
+
+            let hash = cipher.update(request.body.password, 'utf8', 'hex');
+            hash += cipher.final('hex');
+
+            var token  = request.header('xtoken')
+            const secret = JWT_SECRET;
+            const verificationResponse = jwt.verify(token, secret) as DataStoredInToken;
+            const id = verificationResponse.id;
+
+            const updatedEntity = await this.service.modify(id, {hash : hash})
+            response.status(201).send({});
+
+
+        } catch (error) {
+            console.log(error)
+            next(new HttpException(400, error.message))
+        }
     }
 
     addUser = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
@@ -39,6 +71,8 @@ class UserController extends EntityController {
 
             const newEntity = await new UserService().add({ profile, name, hash });
             response.status(200).send(newEntity);
+
+            
         } catch (error) {
             console.log(error)
             next(new HttpException(400, error.message))
